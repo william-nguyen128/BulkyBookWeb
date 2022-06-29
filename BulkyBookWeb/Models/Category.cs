e1c7edc93ad9ccbbe16ba7fd9767bc463273dc2e
william-nguyen128 <84336299+william-nguyen128@users.noreply.github.com>
2022-06-29 12:44:37 +0700

    Add "Category.cs" to Models

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key] // PRIMARY KEY
+        public int Id { get; set; }
+        [Required] // REQUIRED ATTRIBUTE
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
